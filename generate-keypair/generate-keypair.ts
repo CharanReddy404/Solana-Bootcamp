20ae555bb662a1d19836544568ef9817b95b191b
charanreddy404 <charanlucky6143@gmail.com>
2024-08-15 10:25:41 +0530

    solana completed lab 1

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -3,15 +3,18 @@ import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
-
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
 console.log(
   `✅ Finished! We've loaded our secret key securely, using an env file!`
 );
 
 // // Step 2: Generated and Log keypair
+// import base58 from "bs58";
 // import { Keypair } from "@solana/web3.js";
 
 // const keypair = Keypair.generate();
+// console.log(base58.encode(keypair.secretKey));
 
 // console.log(`The public key is: `, keypair.publicKey.toBase58());
 // console.log(`The secret key is: `, keypair.secretKey);
